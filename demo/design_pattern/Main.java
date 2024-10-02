7934e784f15bba7408fe7ab01ab2c13a23c5d3c5
NhatHUY0910 <157088439+NhatHUY0910@users.noreply.github.com>
2024-10-02 09:38:45 +0700

    Delete demo directory

--- demo/design_pattern/Main.java
+++
@@ -1,8 +0,0 @@
-package demo.design_pattern;
-
-public class Main {
-    public static void main(String[] args) {
-        President president = President.getInstance();
-        president.showMessage();
-    }
-}
